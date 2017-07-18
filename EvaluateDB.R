b23ac158e43a31db92d23ae8d22585726952a6bf
FelipeSBarros <felipe.b4rros@gmail.com>
2017-07-18 17:52:15 -0300

    07/18/17 Small changes and testswith a different package 'caroline' to append data to a table without the necessity of inserting the ID

--- EvaluateDB.R
+++ EvaluateDB.R
@@ -2,6 +2,12 @@
 library("RPostgreSQL")
 library(data.table)
 
+# ?PostgreSQL
+# PostgreSQL(max.con = 16, fetch.default.rec = 500, force.reload = FALSE)
+# The following commands must be usefuls to check the result from query!
+# dbHasCompleted(creationQuery)
+# info <- dbGetInfo(creationQuery)
+
 # create a connection
 # save the password that we can "hide" it as best as we can by collapsing it
 pw <- {"postgres"}
@@ -17,10 +23,10 @@ rm(pw) # removes the password
 
 
 if ( # check for the evaluate table
-!dbExistsTable(con, "evaluate")){
+!dbExistsTable(con, "evaluate")) {
   # FALSE
   sql_tableCreate <- "CREATE TABLE evaluate
-(--idevaluate integer PRIMARY KEY,
+(--id integer PRIMARY KEY,
   kappa numeric(12, 10),
   spec_sens numeric(12, 10),
   no_omission numeric(12, 10),  
@@ -40,40 +46,92 @@ if ( # check for the evaluate table
   OWNER TO postgres;"
 dropSQL <- "DROP TABLE public.evaluate;"  
 # sends the command and creates the table
-dbGetQuery(con, sql_tableCreate)
-#dbGetQuery(con, dropSQL)
-}
-
-# if true follow
+# creationQuery <- dbGetQuery(con, sql_tableCreate)
+creationQuery <- dbSendQuery(con, sql_tableCreate)
+# dbGetQuery(con, dropSQL)
+# dropQuery <- dbSendQuery(con, dropSQL)
+} 
 
 # creates df, a data.frame with the necessary columns
-#data(mtcars)
+# The following code won't be necessary ... ----
 evaluate <- list.files("./examples", full.names = TRUE)
 evaluate <- lapply(evaluate, read.table)
 evaluate <- rbindlist(evaluate, use.names = TRUE)
-class(evaluate)
 evaluate <- as.data.frame(evaluate)
 specie <- list.files("./examples")[1]
-
+# #this part is to get the species names. Must be changed acording to modellingprocess ----
 specie <- stringr::str_split(specie, pattern="_")[[1]][1]
 specie <- stringr::str_split(specie, pattern="evaluate")[[1]][2]
 evaluate$specie <- specie
-
-str(evaluate)
 evaluate$algoritmo <- as.character(evaluate$algoritmo)
 
-names(evaluate)[7:8] <- c("auc", "tss")
+names(evaluate) <- tolower(names(evaluate))
 rm(specie)
 
-# writes df to the PostgreSQL database "postgres", table "cartable" 
-dbWriteTable(con, "evaluate", 
-             value = evaluate, append = TRUE, row.names = FALSE)
+# test if evaluate dataframe has same columns from database
+if (!all.equal(colnames(evaluate), dbListFields(con, "evaluate"))){
+  stop("Different columns from database!")
+} else {
+  # if true, append the result to database table
+  # writes df to the PostgreSQL database "postgres"
+  # ?dbWriteTable
+  dbWriteTable(con, "evaluate", value = evaluate, append = TRUE, row.names = FALSE)
+}
 
+# Retrieve data from database
 # query the data from postgreSQL 
 df_postgres <- dbGetQuery(con, "SELECT * from evaluate")
+# df_postgres <- dbSendQuery(con, "SELECT * from evaluate")
 
 # compares the two data.frames
 identical(evaluate, df_postgres)
 # can be FALSE
 all.equal(evaluate, df_postgres) # identifying the differences
+# TRUE
+
+
+# Other methods from appending data ----
+sql_tableCreate <- "CREATE TABLE teste
+(id integer PRIMARY KEY,
+  kappa numeric(12, 10),
+  spec_sens numeric(12, 10),
+  no_omission numeric(12, 10),  
+  prevalence numeric(12, 10),
+  equal_sens_spec numeric(12, 10),
+  sensitivity numeric(12, 10),
+  AUC numeric(12, 10),
+  TSS numeric(12, 10),
+  algoritmo character varying NOT NULL, --#como usar o ID do algoritmo salvo no banco?
+  partition numeric(12, 10),
+  specie character varying NOT NULL
+)
+WITH (
+OIDS=FALSE
+);
+ALTER TABLE teste
+OWNER TO postgres;"
+dropSQL <- "DROP TABLE public.teste;"  
+
+# sends the command and creates the table
+# creationQuery <- dbGetQuery(con, sql_tableCreate)
+creationQuery <- dbSendQuery(con, sql_tableCreate)
+# dbGetQuery(con, dropSQL)
+# dropQuery <- dbSendQuery(con, dropSQL)
+
+# ?dbWriteTable
+# Teste com pacote Caroline
+# install.packages("caroline")
+library(caroline)
+# evaluate$id <- as.integer(rownames(evaluate))
+dbWriteTable2(con, "teste", evaluate, fill.null = FALSE, add.id=FALSE,
+              row.names=FALSE, pg.update.seq=TRUE, append = TRUE)
+
+# Retrieve data from database
+# query the data from postgreSQL 
+df_postgres <- dbGetQuery(con, "SELECT * from teste")
+
+# compares the two data.frames
+identical(evaluate, df_postgres[,2:length(df_postgres)])
+# can be FALSE
+all.equal(evaluate, df_postgres[,2:length(df_postgres)]) # identifying the differences
 # TRUE
